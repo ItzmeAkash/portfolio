fcdd6bdb35db261c5b6c8ac073556d556d7b4cc8
Akash P S <116643229+ItzmeAkash@users.noreply.github.com>
2024-02-25 13:15:37 +0530

    resume pdf downloaded setted

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -3,15 +3,25 @@ import './intro.css'
 import bg from '../../assets/image1.png'
 import {Link} from 'react-scroll'
 import btn from '../../assets/hireme.png'
-
+import resume from '../../assets/Akash Ps - Full Stack Dev.pdf'
 const Intro = () => {
+
+  const handleHireButton = () =>{
+    const link = document.createElement('a');
+    link.href = resume;
+    link.download = 'AkashResume.pdf';
+    document.body.appendChild(link)
+    link.click();
+    document.body.removeChild(link)
+  };
+
   return (
     <section id="intro">
         <div className="introContent">
             <span className="hello">Hello,</span>
             <span className="introText">I'm <span className='introName'>Akash Ps</span><br />Full Stack Developer</span>
             <p className="introPara">I am a skiled Full Stack web developer and Ai Engineer </p>
-            <Link><button className='btn'><img src={btn} alt="" className='btnImg' />Hire Me</button></Link>
+            <Link><button className='btn' onClick={handleHireButton}><img src={btn} alt="" className='btnImg' />Hire Me</button></Link>
         </div>
         <img src={bg} alt="Profile" className='bg' />
     </section>
